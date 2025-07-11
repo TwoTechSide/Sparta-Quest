ce51f682f4bb90b8090e14c3583a1c6b90d11950
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-11 16:38:08 +0900

    fix: 계산기 2단계 내용 수정
    - 불필요한 변수 내용 수정
    - #b64aa2f 내용처럼 예외 처리, 반복문 수정
    - Set 으로 사칙연산 기호 확인

--- src/Ch2Quest/level2/App.java
+++ src/Ch2Quest/level2/App.java
@@ -1,6 +1,8 @@
 package Ch2Quest.level2;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class App {
 
@@ -8,29 +10,52 @@ public class App {
         Calculator c = new Calculator();
         Scanner s = new Scanner(System.in);
 
+        // 사칙 연산이 포함된 Set
+        Set<String> symbolSet = new HashSet<>();
+        symbolSet.add("+");
+        symbolSet.add("-");
+        symbolSet.add("*");
+        symbolSet.add("/");
+
+        String symbol;
+
         while (true) {
             System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 
             String input;
-            int num1 = 0;
-            int num2 = 0;
-            int result;
+            int num1, num2;
 
             /* 숫자 입력 */
-            try {
-                System.out.print("첫 번째 값을 입력해주세요 : ");
-                num1 = s.nextInt();
-                System.out.print("두 번째 값을 입력해주세요 : ");
-                num2 = s.nextInt();
-            } catch (Exception e) {
-                System.out.println("적절한 숫자가 아닙니다. 프로그램을 종료합니다.");
-                throw new RuntimeException(e);
+            while (true) {
+                try {
+                    System.out.print("첫 번째 값을 입력해주세요 : ");
+                    num1 = Integer.parseInt(s.nextLine());
+                    System.out.print("두 번째 값을 입력해주세요 : ");
+                    num2 = Integer.parseInt(s.nextLine());
+
+                    if (num1 < 0 || num2 < 0)
+                        throw new Exception();
+
+                    break;
+                } catch (Exception e) {
+                    System.out.println("적절한 입력이 아닙니다. 다시 입력해주세요.");
+                }
             }
 
             /* 사칙연산 기호 입력 */
-            s.nextLine(); // Scanner 버퍼 비우기
-            System.out.print("연산을 선택해주세요 [+, -, *, /] : ");
-            String symbol = s.nextLine();
+            while (true) {
+                System.out.print("연산을 선택해주세요 [+, -, *, /] : ");
+                String sign = s.nextLine();
+
+                // 정해진 사칙연산을 사용하지 않는 경우 예외 처리
+                try {
+                    if (!symbolSet.contains(sign)) throw new Exception();
+                    symbol = sign;
+                    break;
+                } catch (Exception e) {             // 사칙 연산 입력이 잘못된 경우
+                    System.out.println("올바른 입력이 아닙니다. 사칙연산을 선택해주세요.");
+                }
+            }
 
             /* Calculator 클래스에 입력 후 계산 */
             c.setFirstNum(num1);
@@ -38,7 +63,7 @@ public class App {
             c.setSymbol(symbol);
 
             c.calculate();
-            result = c.getLastCalc();
+            c.getLastCalc();
 
             /* 결과 출력 */
             c.printResultsHistory();
